40f81f8e437235c8b8e8e5ce658be95cc7859b16
Robin Pan <panrobin25@gmail.com>
2025-03-19 13:26:18 -0400

    Updated server.js to pull from MongoDB

--- server/server.js
+++ server/server.js
@@ -15,3 +15,32 @@ app.use(cors()); // Enable CORS for all routes
 const PORT = 3000;
 
 app.use(express.json());
+
+app.get("/products", async (req, res) => {
+  try {
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
+    const products = await collection.find({}).toArray();
+    res.json(products);
+  } catch (err) {
+    console.error("Error:", err);
+    res.status(500).send("Error fetching products");
+  }
+});
+
+app.get("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
+    const products = await collection.find({ id: id }).toArray();
+    res.json(products);
+  } catch (err) {
+    console.error("Error:", err);
+    res
+      .status(500)
+      .send(`Error fetching product with id ${id}`);
+  }
+});
