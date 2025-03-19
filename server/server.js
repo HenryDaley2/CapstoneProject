ae596c82ebe2b206c816f7cdb5600efa084d841b
Robin Pan <panrobin25@gmail.com>
2025-03-19 10:34:19 -0400

    Updated model to use only type and price as features. Updated generator functions to match this new model. Created more accurate json data for use in website

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { promises as fs } from "fs";
+import { MongoClient, ObjectId } from "mongodb";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const url = process.env.MONGO_DB_URL;
+const dbName = process.env.MONGO_DB;
+const collectionName = process.env.MONGO_DB_COLLECTION;
+
+const app = express();
+app.use(cors()); // Enable CORS for all routes
+const PORT = 3000;
+
+app.use(express.json());
