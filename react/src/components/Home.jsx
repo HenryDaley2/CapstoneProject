1a3b8f9bb69e96236b21660ce00901b7e2ee4a97
Henry Daley <henrydaley76@gmail.com>
2025-03-19 10:37:10 -0400

    more stuff

--- react/src/components/Home.jsx
+++ react/src/components/Home.jsx
@@ -1,8 +1,8 @@
 const home = () => {
     return (
         <div style={{ textAlign: "center", marginTop: "50px"}}>
-            <h1> Welcome to the home page</h1>
-            <p> Our Grocery store display products from around the world</p>
+            <h1> Welcome to PURE PICKS</h1>
+            <p> Our Grocery store displays products from around the world!</p>
         </div>
     );
 };
