17c92c2597b3aa96bba69e38ca1dda9cb131103e
Henry Daley <henrydaley76@gmail.com>
2025-03-20 09:54:45 -0400

    navbar final fixes

--- react/src/components/Navbar.jsx
+++ react/src/components/Navbar.jsx
@@ -1,10 +1,6 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
-
 
 const Navbar = () => {
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-
   return (
     <nav className="navbar">
       {/* Left Section - App Name */}
@@ -20,45 +16,36 @@ const Navbar = () => {
           About
         </Link>
 
-        {/* Dropdown Menu */}
-        <div
-          className="dropdown-container"
-          onMouseEnter={() => setDropdownOpen(true)}
-          onMouseLeave={() => setDropdownOpen(false)}
-        >
+        <div className="dropdown-container">
           <button className="dropdown-button">Menu ▼</button>
-          {dropdownOpen && (
-            <div className="dropdown-menu">
-              <Link to="/category/fruits" className="dropdown-item">
-                Fruits
-              </Link>
-              <Link to="/category/vegetables" className="dropdown-item">
-                Vegetables
-              </Link>
-              <Link to="/category/dairy" className="dropdown-item">
-                Dairy
-              </Link>
-              <Link to="/category/meats" className="dropdown-item">
-                Meats
-              </Link>
-              <Link to="/category/seafood" className="dropdown-item">
-                Seafood
-              </Link>
-              <Link to="/category/bread" className="dropdown-item">
-                Bread
-              </Link>
-            </div>
-          )}
+          <div className="dropdown-menu">
+            <Link to="/category/fruits" className="dropdown-item">
+              Fruits
+            </Link>
+            <Link to="/category/vegetables" className="dropdown-item">
+              Vegetables
+            </Link>
+            <Link to="/category/dairy" className="dropdown-item">
+              Dairy
+            </Link>
+            <Link to="/category/meats" className="dropdown-item">
+              Meats
+            </Link>
+            <Link to="/category/seafood" className="dropdown-item">
+              Seafood
+            </Link>
+            <Link to="/category/bread" className="dropdown-item">
+              Bread
+            </Link>
+          </div>
         </div>
       </div>
 
-      {/* Right Section - Create Account Button */}
+      {/* Right Section - Login & Create Account */}
       <div className="navbar-right">
-        <Link to="/createaccount" className="create-account-btn">
+        <Link to="/create-account" className="create-account-btn">
           Create Account
         </Link>
-      </div>
-      <div className="navbar-right">
         <Link to="/login" className="login-btn">
           Login
         </Link>
