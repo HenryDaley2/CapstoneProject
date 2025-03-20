adc80541053f5fd0e5591d922a6ff8043e98c4b7
Robin Pan <panrobin25@gmail.com>
2025-03-20 09:41:37 -0400

    Added links to each of the product cards

--- react/src/components/product.jsx
+++ react/src/components/product.jsx
@@ -3,7 +3,11 @@ import React from "react";
 const Product = (props) => {
   return (
     <div className="product-card">
-      <h3>{props.data.item}</h3>
+      <h3>
+        <a href={`${import.meta.env.VITE_STORE_URL}/${props.data.id}`}>
+          {props.data.item}
+        </a>
+      </h3>
       <p> {props.data.type}</p>
       <p>Price: ${props.data.price.toFixed(2)}</p>
     </div>
